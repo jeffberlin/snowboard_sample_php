7dada01700c3a13610ade38560e02e6afd482563
Jeff Berlin <jeff@jbrally.com>
2017-11-30 17:27:05 -0500

    Building page

--- index.php
+++ index.php
@@ -21,32 +21,137 @@
 		}
 
 		.container-fluid {
+			margin-top: 10vh;
+			max-width: 1200px;
+			background: rgba(0, 0, 0, 0.8);
+			min-height: 100vh;
 			padding: 0;
 		}
 
-		.container-fluid {
-			margin-top: 10vh;
-			max-width: 1200px;
-			background: rgba(251, 251, 251, 0.5);
-			height: 100vh;
+		h1 {
+			font-family: rooney-sans, sans-serif;
+			font-style: normal;
+			font-weight: 500;
+			letter-spacing: .75px;
+			font-size: 38pt;
+			text-shadow: 0 0 2px #9b9b9b;
+			color: #ffffff;
+		}
+
+		h2 {
+			font-family: rooney-sans, sans-serif;
+			color: #ffffff;
+			text-align: left;
+			letter-spacing: .8px;
+			
 		}
 
 		.header {
 			padding-top: 2rem;
+		}
+
+		.logo-white {
+			margin-right: 1.5rem;
+		}
+
+		.snowboarder {
+			width: 15rem;
+		}
+
+		.header h2 {
+			padding-left: 3rem;
+			margin-top: 4rem;
+			margin-bottom: 1.5rem;
+		}
+
+		.boards {
+			display: inline-block;
+			margin-bottom: 2rem;
+		}
+
+		.boards img {
+			height: 20rem;
+		}
+
+		.snowboard-images {
+			background: rgba(251,251,251, .6);
+			padding-top: 2rem;
+			padding-bottom: 2rem;
 		}
 		
 		
 	</style>
 	<body>
 
-		<div class="header container-fluid text-center">
+		<?php
 
-			<img class="img-fluid text-center" src="images/logo.png">
+			$mensBoards = "Men's Boards";
 
-			<?php
+		?>
 
+		<div class="container-fluid">
 
-			?>
+			<div class="header text-center">
+
+				<img class="img-fluid logo-white" src="images/logo_white.png">
+
+				<img class="img-fluid snowboarder" src="images/snowboarder.png">
+
+				<h2><?php echo $mensBoards; ?></h2>
+
+				<div class="snowboard-images">
+
+					<div class="boards col-lg-3 col-md-3">
+
+						<img class="img-fluid" src="images/board-1.png">
+
+						<h3>Process Flying V</h3>
+
+					</div>
+
+					<div class="boards col-lg-3 col-md-3">
+
+						<img class="img-fluid" src="images/board-3.png">
+
+						<h3>Talent (women's)</h3>
+
+					</div>
+
+					<div class="boards col-lg-3 col-md-3">
+
+						<img class="img-fluid" src="images/board-2.png">
+
+						<h3>Clash</h3>
+
+					</div>
+
+					<div class="boards col-lg-3 col-md-3">
+
+						<img class="img-fluid" src="images/board-4.png">
+
+						<h3>Custom X Flying V</h3>
+
+					</div>
+
+					<div class="boards col-lg-3 col-md-3">
+
+						<img class="img-fluid" src="images/board-5.png">
+
+						<h3>Chopper LTD Marvel</h3>
+
+					</div>
+
+					<div class="boards col-lg-3 col-md-3">
+
+						<img class="img-fluid" src="images/board-5.png">
+
+						<h3>Chopper LTD Marvel</h3>
+
+					</div>
+
+				</div>
+
+			</div>
 
 		</div>
 
